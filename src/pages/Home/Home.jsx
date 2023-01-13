5c513181b2afe067b886427e889f4f2bbadd8bd7
Quynh Huong Pham <qhuong1008@gmail.com>
2023-01-13 14:59:36 +0700

    chart, featured design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import "./Home.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
+import Widget from "../../components/Widget/Widget";
+import Featured from "../../components/Featured/Featured";
+import Chart from "../../components/Chart/Chart";
+
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="home-container">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );
